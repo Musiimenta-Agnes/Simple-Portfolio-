14b1985bdd38f2de77731b595e74c68ad584a47e
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2025-04-24 18:24:26 +0300

    changes

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// The code to allow automatic typing of words on page load
+const text = "WEB & GRAPHIC DESIGNER";
+const element = document.getElementById("myText");
+let i = 0;
+
+function type() {
+  element.classList.add("show");
+  if (i < text.length) {
+    element.textContent += text.charAt(i);
+    i++;
+    setTimeout(type, 100); // 100ms delay between letters
+  }
+}
+
+window.onload = type;
+
+
+
+
+
+
+// const myWord = "Frontend Developer";
+// const myElement = document.getElementById("word");
+// let a = 0;
+
+// function type() {
+//   myElement.classList.add("show");
+//   if (i < myWord.length) {
+//     myElement.textContent += myWord.charAt(a);
+//     a++;
+//     setTimeout(type, 100); // 100ms delay between letters
+//   }
+// }
+
+// window.onload = type;
